07c21a2f9292c3bd87e2718845640950a3c67b6c
Colten Krieg <coltenkrieg@calvisitor-10-105-146-159.calvisitor.1918.berkeley.edu>
2018-04-17 20:20:07 -0700

    working html with yelp api

--- yelpGO.js
+++ yelpGO.js
@@ -1,13 +1,8 @@
-<<<<<<< HEAD
-   $("#submit2").on('click', function(event) {
+   $("#submit0").on('click', function(event) {
    event.preventDefault()
 
+
     let token = "Bearer Fb_cwVKg4ODNzdtxziO3Yj6NC_RnW498C6_Tk1svho-TzraerxvKIGBfymZfR-3kE8jpYqic6l6cp2shLrkNypGr0H0XV_ghVNqfSy_Wo7QqRN_iR-w-6LZYn6vWWnYx"
-=======
-   $( document ).ready(function() {
-   
-    let token = "Bearer XCKbzZHW8v3vXJbl2iouO9IGz2IhnrLPb6_UHmjkLy_QIk2P5w5UIqvK-0CNM7OzLvYbP1rbccFC9tWTzVY5YKc-PpHAA55bllNwVhSzWi47ndPnK8F1-MprbBPQWnYx"
->>>>>>> 02264ced7e8b8347719ed46565e9ce3dcdf2a0b2
     let search = "https://api.yelp.com/v3/businesses/search"
     let corsAnywhere = "https://cors-anywhere.herokuapp.com/"
     var zip = $("#zip").val()
@@ -34,11 +29,7 @@
             $("#content-holder").prepend('<div> <h1> <span id="info"> </span> <h1> </div>');
 
             var url = response.businesses[i].url
-<<<<<<< HEAD
             var namme = response.businesses[i].name
-=======
-            var namme = response.businesse[i].name
->>>>>>> 02264ced7e8b8347719ed46565e9ce3dcdf2a0b2
             
             var namer = $('<a href="' + url + '"> ' + namme + ' </a>');
             var rating = $("<div>").append(response.businesses[i].rating)
